b8745a353e06a5c3915e14902bcd4e0ffe2281bf
Dierlisson Justiniano <dierlisson1@gmail.com>
2025-01-04 22:39:02 -0300

    create adapter

--- app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -1,4 +1,39 @@
 package com.devspace.recyclerview
 
-class ContactListAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+// Adaptacao entre o data class e o item_list layout
+class ContactListAdapter :
+    ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    // view holder = view que segura os dados
+    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    // compara a diferenca quando nossa lista é atualizada
+    class ContactDiffUtils : DiffUtil.ItemCallback<Contact>() {
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+    }
+
+
 }
