f35d1993b9f3253ce00750498586064ba286a007
Dierlisson Justiniano <dierlisson1@gmail.com>
2025-01-07 21:04:47 -0300

    Interagindo com RecycleView na Activity

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -20,14 +21,17 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // Create data class
-        // Create List from data class type
-        // Create Adapter
+
         // Set Adapter
         // Linear Layout manager
+        // submeter a lista
 
         val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = ContactListAdapter()
 
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
 
 
     }
@@ -38,63 +42,63 @@ val contacts = listOf(
         "Dierlisson",
         "(55) 27 999999999",
         R.drawable.sample2
-    ),Contact(
+    ), Contact(
         "Leticia",
         "(55) 27 999123499",
         R.drawable.sample3
-    ),Contact(
+    ), Contact(
         "Marcos",
         "(55) 27 129837732",
         R.drawable.sample12
-    ),Contact(
+    ), Contact(
         "Vitor",
         "(55) 27 123123123",
         R.drawable.sample14
-    ),Contact(
+    ), Contact(
         "Josefina",
         "(55) 27 981723981",
         R.drawable.sample1
-    ),Contact(
+    ), Contact(
         "Ana",
         "(55) 27 999876543",
         R.drawable.sample4
-    ),Contact(
+    ), Contact(
         "Carla",
         "(55) 27 987654321",
         R.drawable.sample5
-    ),Contact(
+    ), Contact(
         "Fernanda",
         "(55) 27 986543210",
         R.drawable.sample6
-    ),Contact(
+    ), Contact(
         "Gabriela",
         "(55) 27 985432109",
         R.drawable.sample7
-    ),Contact(
+    ), Contact(
         "Heleno",
         "(55) 27 984321098",
         R.drawable.sample8
-    ),Contact(
+    ), Contact(
         "Igor",
         "(55) 27 983210987",
         R.drawable.sample9
-    ),Contact(
+    ), Contact(
         "Juliano",
         "(55) 27 982109876",
         R.drawable.sample10
-    ),Contact(
+    ), Contact(
         "Leonarda",
         "(55) 27 981098765",
         R.drawable.sample11
-    ),Contact(
+    ), Contact(
         "Mariana",
         "(55) 27 980987654",
         R.drawable.sample13
-    ),Contact(
+    ), Contact(
         "Paula",
         "(55) 27 979876543",
         R.drawable.sample15
-    ),Contact(
+    ), Contact(
         "Ricarda",
         "(55) 27 978765432",
         R.drawable.sample16
